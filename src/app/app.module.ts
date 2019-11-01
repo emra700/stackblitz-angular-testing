b5eed409ba010e27049e4106415cdd7ff03dcc7f
emra700 <emil.ramstedt@gmail.com>
2019-11-01 14:27:00 +0100

    1 file modified, 10 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, 
+  ReactiveFormsModule,
+   RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+       { path: 'products/:productId', component: ProductDetailsComponent },
+    ])
+  ],
+  declarations: [ AppComponent, HelloComponent, ProductListComponent, ProductAlertsComponent, ProductDetailsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
